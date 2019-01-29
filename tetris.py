936037c00915f7ea8eb41289338e15022673f4ba
Ratatat Cat <somethingtrippy@yahoo.com>
2019-01-28 20:07:56 -0600

    Add convert shape function

--- tetris.py
+++ tetris.py
@@ -174,7 +174,16 @@ def convert_shape_format(shape):
 
 
 def valid_space(shape, grid):
-    pass
+    accepted_pos = [[(j,i) for j in range(10) if grid[i][j] == [(0,0,0)]] for i in range(20)]
+    accepted_pos = [j for sub in accepted_pos for j in sub]
+
+    formatted = convert_shape_format(shape)
+
+    for pos in formatted:
+        if pos not in accepted_pos:
+            if pos[1] > -1:
+                return False
+    return True
 
 
 def check_lost(positions):
