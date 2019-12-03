fff59b428f293ce7ecfc77ecf4bbbfb36dc58343
ratatatcat <iretoncat@gmail.com>
2019-12-02 18:37:04 -0600

    Tetris updates

--- tetris.py
+++ tetris.py
@@ -172,6 +172,8 @@ def convert_shape_format(shape):
     for i, pos in enumerate(positions):
         positions[i] = (pos[0] - 2, pos[1] - 4)
 
+    return positions
+
 
 def valid_space(shape, grid):
     accepted_pos = [[(j,i) for j in range(10) if grid[i][j] == [(0,0,0)]] for i in range(20)]
@@ -198,14 +200,13 @@ def draw_text_middle(text, size, color, surface):
     pass
 
 
-def draw_grid(surface, grid):
+def draw_grid(surface, row, col):
     sx = top_left_x
     sy = top_left_y
-
-    for i in range(len(grid)):
-        pygame.draw.line(surface, (128,128,128), (sx, sy+i*block_size), (sx+play_width, sy+i*block_size))
-        for j in range(len(grid[i])):
-            pygame.draw.line(surface, (128,128,128), (sx, j*block_size, sy), (sx, j*block_size, sy+play_height))
+    for i in range(row):
+        pygame.draw.line(surface, (128,128,128), (sx, sy+ i*30), (sx + play_width, sy + i * 30))  # horizontal lines
+        for j in range(col):
+            pygame.draw.line(surface, (128,128,128), (sx + j * 30, sy), (sx + j * 30, sy + play_height))  # vertical lines
 
 
 def clear_rows(grid, locked):
@@ -231,7 +232,7 @@ def draw_window(surface, grid):
 
     pygame.draw.rect(surface, (255,0,0), (top_left_x, top_left_y, play_width, play_height), 4)
 
-    draw_grid(surface, grid)
+    draw_grid(surface, 20, 10)
     pygame.display.update()
 
 def main(win):
@@ -242,33 +243,68 @@ def main(win):
     run = True
     current_piece = get_shape()
     next_piece = get_shape()
-    clock = pygame.time.clock()
+    clock = clock = pygame.time.Clock()
+
     fall_time = 0
 
     while run:
+        fall_speed = 0.27
+
+        grid = create_grid(locked_positions)
+        fall_time += clock.get_rawtime()
+        clock.tick()
+
+        if fall_time/1000 > fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            if not(valid_space(current_piece, grid)) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+        
         for event in pygame.event.get():
-            if event.type == pygame.event.QUIT:
+            if event.type == pygame.QUIT:
                 run = False
 
-            if event.type == pygame.event.KEYDOWN:
-                if event.key == pygame.event.K_LEFT:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
                     current_piece.x -= 1
                     if not (valid_space(current_piece, grid)):
                         current_piece.x += 1
-                if event.key == pygame.event.K_RIGHT:
+                if event.key == pygame.K_RIGHT:
                     current_piece.x += 1
                     if not (valid_space(current_piece, grid)):
                         current_piece.x -= 1
-                if event.key == pygame.event.K_DOWN:
+                if event.key == pygame.K_DOWN:
                     current_piece.y += 1
                     if not (valid_space(current_piece, grid)):
                         current_piece.y -= 1         
-                if event.key == pygame.event.K_UP:
+                if event.key == pygame.K_UP:
                     current_piece.rotation += 1
                     if not(valid_space(current_piece, grid)):
                         current_piece.rotation -= 1
 
+        shape_pos = convert_shape_format(current_piece)
+
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[i]
+            if y > -1:
+                grid[y][x] = current_piece.color
+
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0],pos[1])
+                locked_positions[p] = current_piece.color
+        current_piece = next_piece
+        next_piece = get_shape()
+        change_piece = False
+
         draw_window(win, grid)
+
+        if(check_lost(locked_positions)):
+            run = False;
+
+
+        pygame.display.quit();
 
 def main_menu(win):  # *
     main(win)
